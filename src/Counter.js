31d6bf30c8f6ef27141389c725ccae1e0695dc00
piwe2004 <piwe2004@gmail.com>
2021-04-19 13:57:44 +0900

    useReducer 작업

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(0);
+
+   /* const [number, setNumber] = useState(0);
+
     const onIncrease = () => {
-        setNumber(number +1)
+        setNumber(number +1);
     }
+
     const onDecrease = () => {
-        setNumber(number -1)
-    }
+        setNumber(number -1);
+    }*/
+
+    const [number, dispatch] = useReducer(reducer, 0);
+
+    const onIncrease = () => {
+        dispatch({ type: 'INCREMENT' });
+    };
+
+    const onDecrease = () => {
+        dispatch({ type: 'DECREMENT' });
+    };
+
     return (
       <div>
           <h1>{number}</h1>
