eaba24bd801c9400e2eaa16e7afc5839ef82281d
piwe2004 <piwe2004@gmail.com>
2021-04-20 10:24:49 +0900

    ContextApi 연습2

--- src/App.js
+++ src/App.js
@@ -94,26 +94,12 @@ function App() {
        nextId.current += 1;
     }, [username, email]);
 
-    const onToggle = useCallback(id => {
-        dispatch({
-            type: 'TOGGLE_USER',
-            id
-        });
-    }, []);
-
-    const onRemove = useCallback(id => {
-        dispatch({
-            type:'REMOVE_USER',
-            id
-        });
-    }, []);
-
     const count = useMemo(() => countActiveUsers(users), [users]);
 
     return (
         <UserDispatch.Provider value={dispatch}>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-            <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+            <UserList users={users} />
             <div>활성사용자 수 : {count}</div>
         </UserDispatch.Provider>
     );
